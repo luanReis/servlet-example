a6755cdb98565d06f23f1c070fc8114a5239e6b3
Luan Reis <luan.rpsantos@gmail.com>
2017-04-05 14:11:24 +0200

    Initial servlet setup

---
+++ src/main/java/com/luanreis/MyServlet.java
@@ -0,0 +1,19 @@
+package com.luanreis;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws
+            ServletException, IOException {
+
+        PrintWriter writer = response.getWriter();
+        writer.println("Hello World!!!");
+    }
+}
